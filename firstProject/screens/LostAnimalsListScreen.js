e83eb8be3a3455b32e640125d199b432e55e7f47
martarensal <martarendon180497@gmail.com>
2022-01-31 20:30:39 +0100

    add date to MyLostAnimal and LostAnimalList

--- firstProject/screens/LostAnimalsListScreen.js
+++ firstProject/screens/LostAnimalsListScreen.js
@@ -31,13 +31,11 @@ export default class LostAnimalsListScreen extends React.Component {
       height: 80,
     };
   }
-   showMoreLostAnimals() {
-    this.setState({page: this.state.page + 1}, () =>
-      this.fetchPublications(),
-    );
+  showMoreLostAnimals() {
+    this.setState({page: this.state.page + 1}, () => this.fetchPublications());
   }
   handleGetPublicationResponse(response) {
-     response.json().then(data => 
+    response.json().then(data =>
       this.setState({
         publications: this.state.publications.concat(data.pages),
         paginationInfo: data.paginationInfo,
@@ -47,71 +45,75 @@ export default class LostAnimalsListScreen extends React.Component {
   }
 
   fetchPublications() {
-      SecurityUtils.authorizeApi(
-        [this.state.page, 6],
-        getPaginatedPublications,
-      ).then(this.handleGetPublicationResponse.bind(this));
+    SecurityUtils.authorizeApi(
+      [this.state.page, 6],
+      getPaginatedPublications,
+    ).then(this.handleGetPublicationResponse.bind(this));
   }
 
   componentDidMount() {
     this.fetchPublications();
   }
+  getCurrentDate = () => {
+    var date = new Date().getDate();
+    var month = new Date().getMonth() + 1;
+    var year = new Date().getFullYear();
+    return date + '-' + month + '-' + year;
+  };
 
   render() {
-      return (
-        <>
-          <Appbar style={styles.barra}>
-            <Appbar.Action
-              icon="menu"
-              onPress={() =>
-                this.props.navigation.dispatch(DrawerActions.openDrawer())
-              }
-            />
-            <Text style={styles.logo}>SavePet</Text>
-          </Appbar>
-          <ScrollView style={styles.background}>
-            <View style={styles.container}>
-              <Text style={styles.title}> Animales perdidos</Text>
+    return (
+      <>
+        <Appbar style={styles.barra}>
+          <Appbar.Action
+            icon="menu"
+            onPress={() =>
+              this.props.navigation.dispatch(DrawerActions.openDrawer())
+            }
+          />
+          <Text style={styles.logo}>SavePet</Text>
+        </Appbar>
+        <ScrollView style={styles.background}>
+          <View style={styles.container}>
+            <Text style={styles.title}> Animales perdidos</Text>
 
-              {this.state.publications.map(publication => {
-                return (
-                  <Card key={publication.id}>
-                    <Divider style={styles.divider} />
+            {this.state.publications.map(publication => {
+              return (
+                <Card key={publication.id}>
+                  <Divider style={styles.divider} />
 
-                    <Card.Title
-                      style={styles.cardStyle}
-                      title={
-                        publication.publicationDate
-                        /* .toISOString()
-                      .substring(0, '####-##-##'.length)*/
-                      }
-                      titleStyle={styles.title}
-                      subtitle={publication.description}
-                      subtitleStyle={styles.subtitle}
-                      left={() => (
-                        <Image
-                          style={{
-                            width: this.state.width,
-                            height: this.state.height,
-                            borderRadius: 5,
-                          }}
-                          source={{uri: this.state.base64 + publication.image}}
-                        />
-                      )}
-                    />
-                  </Card>
-                );
-              })}
-               {this.state.page !== this.state.paginationInfo.totalPages - 1 &&
+                  <Card.Title
+                    style={styles.cardStyle}
+                    title={this.getCurrentDate(publication.publicationDate)}
+                    titleStyle={styles.title}
+                    subtitle={publication.description}
+                    subtitleStyle={styles.subtitle}
+                    left={() => (
+                      <Image
+                        style={{
+                          width: this.state.width,
+                          height: this.state.height,
+                          borderRadius: 5,
+                        }}
+                        source={{uri: this.state.base64 + publication.image}}
+                      />
+                    )}
+                  />
+                </Card>
+              );
+            })}
+            {this.state.page !== this.state.paginationInfo.totalPages - 1 &&
             this.state.paginationInfo.totalElements !== 0 ? (
-              <Button color="#F05524" onPress={this.showMoreLostAnimals.bind(this)}>
+              <Button
+                color="#F05524"
+                onPress={this.showMoreLostAnimals.bind(this)}>
                 MOSTRAR MÁS
               </Button>
             ) : undefined}
-            </View>
-          </ScrollView>
-        </>
-      );
+          </View>
+        </ScrollView>
+      </>
+    );
   }
 }
 
@@ -164,9 +166,9 @@ const styles = StyleSheet.create({
     fontFamily: 'RobotoSlab-Regular',
     color: '#575757',
     fontSize: 22,
+    marginLeft: 40,
     marginBottom: 15,
     marginTop: 20,
-    textAlign: 'center',
   },
   image: {
     width: 30,
--- firstProject/screens/MyLostAnimals.js
+++ firstProject/screens/MyLostAnimals.js
@@ -14,7 +14,10 @@ import LoadingIndicator from '../components/LoadingIndicator';
 import {ScrollView} from 'react-native-gesture-handler';
 import {DrawerActions} from '@react-navigation/native';
 var SecurityUtils = require('../utils/SecurityUtils');
-import {getPaginatedPublicationsFromUser,deletePublication} from '../client/AnimalApi';
+import {
+  getPaginatedPublicationsFromUser,
+  deletePublication,
+} from '../client/AnimalApi';
 var SecurityUtils = require('../utils/SecurityUtils');
 
 export default class MyLostAnimals extends React.Component {
@@ -33,12 +36,14 @@ export default class MyLostAnimals extends React.Component {
     };
 
     this.deletePublication = this.deletePublication.bind(this);
-    this.handleDetelePublicationResponse = this.handleDetelePublicationResponse.bind(this)
-    this.fetchUserDataWithPublications = this.fetchUserDataWithPublications.bind(this)
+    this.handleDetelePublicationResponse =
+      this.handleDetelePublicationResponse.bind(this);
+    this.fetchUserDataWithPublications =
+      this.fetchUserDataWithPublications.bind(this);
   }
 
   handleDetelePublicationResponse(response) {
-      if (response.ok) {
+    if (response.ok) {
       this.setState({publications: [], page: 0});
       this.fetchUserDataWithPublications();
     } else {
@@ -78,7 +83,7 @@ export default class MyLostAnimals extends React.Component {
   }
 
   fetchUserDataWithPublications() {
-     if (this.state.page === 0) this.setState({loading: true});
+    if (this.state.page === 0) this.setState({loading: true});
     SecurityUtils.tokenInfo().then(info => {
       SecurityUtils.authorizeApi([info.sub], getUser).then(
         this.handleGetUserResponse.bind(this),
@@ -90,131 +95,144 @@ export default class MyLostAnimals extends React.Component {
       'focus',
       this.fetchUserDataWithPublications.bind(this),
     );
+    this._outOfFocus = this.props.navigation.addListener('blur', () =>
+      this.setState({publications: [], page: 0}),
+    );
   }
 
   componentWillUnmount() {
     this._unsubscribe();
   }
 
+  getCurrentDate = () => {
+    var date = new Date().getDate();
+    var month = new Date().getMonth() + 1;
+    var year = new Date().getFullYear();
+    return date + '-' + month + '-' + year;
+  };
   render() {
-     if (this.state.loading) {
+    if (this.state.loading) {
       return <LoadingIndicator />;
     } else {
-    return (
-      <>
-        <Appbar style={styles.barra}>
-          <Appbar.Action
-            icon="menu"
-            onPress={() =>
-              this.props.navigation.dispatch(DrawerActions.openDrawer())
-            }
-          />
-          <Text style={styles.logo}>SavePet</Text>
-        </Appbar>
-       
-        <ScrollView style={styles.background}>
-          <View style={styles.container}>
-            <Text style={styles.title}> Mis animales perdidos</Text>
-             {this.state.publications[0] === undefined ? (
-              <>
-                <View style={styles.container}>
-                  <Text style={styles.label}>
-                    Parece que no has añadido ninguna publicación
-                  </Text>
-                  <TouchableOpacity
-                    onPress={() =>
-                      this.props.navigation.navigate('Añadir animal perdido', {
-                        username: this.state.user.username,
-                      })
-                    }>
-                    <Text style={styles.link}>
-                      ¿Quieres añadir un animal perdido ahora?
+      return (
+        <>
+          <Appbar style={styles.barra}>
+            <Appbar.Action
+              icon="menu"
+              onPress={() =>
+                this.props.navigation.dispatch(DrawerActions.openDrawer())
+              }
+            />
+            <Text style={styles.logo}>SavePet</Text>
+          </Appbar>
+
+          <ScrollView style={styles.background}>
+            <View style={styles.container}>
+              <Text style={styles.title}> Mis animales perdidos</Text>
+              {this.state.publications[0] === undefined ? (
+                <>
+                  <View style={styles.container}>
+                    <Text style={styles.label}>
+                      Parece que no has añadido ninguna publicación
                     </Text>
-                  </TouchableOpacity>
-                </View>
-              </>
-            ) : ( 
-              this.state.publications.map(publication => {
-              return (
-                <Card key={publication.id}>
-                  <Divider style={styles.divider} />
+                    <TouchableOpacity
+                      onPress={() =>
+                        this.props.navigation.navigate(
+                          'Añadir animal perdido',
+                          {
+                            username: this.state.user.username,
+                          },
+                        )
+                      }>
+                      <Text style={styles.link}>
+                        ¿Quieres añadir un animal perdido ahora?
+                      </Text>
+                    </TouchableOpacity>
+                  </View>
+                </>
+              ) : (
+                this.state.publications.map(publication => {
+                  return (
+                    <Card key={publication.id}>
+                      <Divider style={styles.divider} />
 
-                  <Card.Title
-                    style={styles.cardStyle}
-                    title={
-                      publication.publicationDate
-                      /* .toISOString()
-                      .substring(0, '####-##-##'.length)*/
-                    }
-                    titleStyle={styles.title}
-                    subtitle={publication.description}
-                    subtitleStyle={styles.subtitle}
-                    left={() => (
-                      <Image
-                        style={{
-                          width: this.state.width,
-                          height: this.state.height,
-                          borderRadius: 5,
-                        }}
-                        source={{uri: this.state.base64 + publication.image}}
+                      <Card.Title
+                        style={styles.cardStyle}
+                        title={this.getCurrentDate(publication.publicationDate)}
+                        titleStyle={styles.title}
+                        subtitle={publication.description}
+                        subtitleStyle={styles.subtitle}
+                        left={() => (
+                          <Image
+                            style={{
+                              width: this.state.width,
+                              height: this.state.height,
+                              borderRadius: 5,
+                            }}
+                            source={{
+                              uri: this.state.base64 + publication.image,
+                            }}
+                          />
+                        )}
                       />
-                    )}
-                  />
-                  <Card.Actions>
-                      <Button 
-                        color="#E67E00"
-                        onPress={() =>
-                          this.props.navigation.navigate('EditPublicationScreen', {
-                            publication: publication,
-                          })
-                        }>
-                        Editar
-                      </Button>
-                      <Button 
-                        color="red"
-                        onPress={() =>
-                          Alert.alert(
-                            'Confirmación',
-                            '¿Está seguro de que quiere borrar el animal? Los animales eliminados no pueden recuperarse.',
-                            [
+                      <Card.Actions>
+                        <Button
+                          color="#E67E00"
+                          onPress={() =>
+                            this.props.navigation.navigate(
+                              'EditPublicationScreen',
                               {
-                                text: 'Cancelar',
-                                onPress: () => console.log('Cancel Pressed'),
-                                style: 'cancel',
+                                publication: publication,
                               },
-                              {
-                                text: 'Si, estoy seguro',
-                                onPress: () => this.deletePublication(publication.id),
-                              },
-                            ],
-                            {cancelable: false},
-                          )
-                        }>
-                        Eliminar
-                      </Button>
-                    </Card.Actions>
-                  </Card>
-               );
-              })
-            )}
-            {this.state.page !== this.state.paginationInfo.totalPages - 1 &&
-            this.state.paginationInfo.totalElements !== 0 ? (
-              <Button color="#F05524" onPress={this.showMoreLostAnimals.bind(this)}>
-                MOSTRAR MÁS
-              </Button>
-            ) : undefined}
-            
-          </View>
-        </ScrollView>
-      </>
-    );
-  }
+                            )
+                          }>
+                          Editar
+                        </Button>
+                        <Button
+                          color="red"
+                          onPress={() =>
+                            Alert.alert(
+                              'Confirmación',
+                              '¿Está seguro de que quiere borrar el animal? Los animales eliminados no pueden recuperarse.',
+                              [
+                                {
+                                  text: 'Cancelar',
+                                  onPress: () => console.log('Cancel Pressed'),
+                                  style: 'cancel',
+                                },
+                                {
+                                  text: 'Si, estoy seguro',
+                                  onPress: () =>
+                                    this.deletePublication(publication.id),
+                                },
+                              ],
+                              {cancelable: false},
+                            )
+                          }>
+                          Eliminar
+                        </Button>
+                      </Card.Actions>
+                    </Card>
+                  );
+                })
+              )}
+              {this.state.page !== this.state.paginationInfo.totalPages - 1 &&
+              this.state.paginationInfo.totalElements !== 0 ? (
+                <Button
+                  color="#F05524"
+                  onPress={this.showMoreLostAnimals.bind(this)}>
+                  MOSTRAR MÁS
+                </Button>
+              ) : undefined}
+            </View>
+          </ScrollView>
+        </>
+      );
     }
-
+  }
 }
 
 const styles = StyleSheet.create({
- 
   container: {
     flex: 1,
     backgroundColor: 'white',
@@ -223,7 +241,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'white',
   },
- 
+
   barra: {
     backgroundColor: '#E67E00',
   },
@@ -260,7 +278,7 @@ const styles = StyleSheet.create({
     fontSize: 22,
     marginBottom: 15,
     marginTop: 20,
-    textAlign: 'center',
+    marginLeft: 40,
   },
   image: {
     width: 30,
@@ -268,7 +286,7 @@ const styles = StyleSheet.create({
     marginRight: 10,
     marginTop: 30,
   },
-   label: {
+  label: {
     marginLeft: 25,
     marginVertical: 15,
   },
